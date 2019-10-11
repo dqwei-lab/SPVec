798c73bf7a4e9febe2a15e18261c4e8269ed272d
EmiliaZhang <34087900+EmiliaZhang@users.noreply.github.com>
2019-10-11 13:17:23 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
+"""
+#filename:models.py
+# - * - coding:UTF - 8 -* -
+#! /usr/bin/python
+__author__ = "Yu-Fang Zhang" 
+"""
+
+
+
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
